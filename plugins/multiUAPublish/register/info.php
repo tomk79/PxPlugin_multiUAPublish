9ce1d7aa1d3f945a1794e82e411b0aa18b120ec6
Tomoya Koyanagi <tomk79@gmail.com>
2014-03-12 16:58:36 +0900

    initial commit.

---
+++ plugins/multiUAPublish/register/info.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * PX Plugin "multiUAPublish"
+ */
+class pxplugin_multiUAPublish_register_info{
+
+	/**
+	 * プラグインのバージョン情報を取得する
+	 * @return string バージョン番号を示す文字列
+	 */
+	public function get_version(){
+		return '0.0.1a1-nb';
+	}
+
+}
+
+?>
